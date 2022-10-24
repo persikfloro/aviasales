ccdb6d61511f28ede0399df5ec600d79da4317b5
Надя Трансвалева <liberavivi888@gmail.com>
2022-10-24 08:20:16 +0300

    Доработка 1 - добавлена папка SRС и в нее перенесен main.java, gitignore, удалена из отслеживания out

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+
+        int mile = 20; // цена 1 мили
+        int ticket1 = 5000; // цена билета из Санкт-Петербурга в Москву"
+
+        System.out.println("Марии начислено " + (ticket1 / mile) + " миль за билет из Санкт-Петербурга в Москву, стоимостью "+ (ticket1) + " рублей");
+    }
+}
